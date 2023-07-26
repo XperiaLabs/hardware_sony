f9d12b064600a2d2e8a15a9eb7fa0992b3463c99
saku-bruh <notwolfyy@gmail.com>
2023-07-26 19:04:40 +0300

    parts: redesign battery care menu
    * Changed Battery Care icon to be more closer to stock (with a bit of a modern twist)
    * Battery Care modernized preview image & modernized menu icon by @JeyKul
    
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- XperiaParts/src/org/lineageos/settings/device/charger/ChargerSettingsActivity.kt
+++ XperiaParts/src/org/lineageos/settings/device/charger/ChargerSettingsActivity.kt
@@ -7,19 +7,24 @@ package org.lineageos.settings.device.charger
 
 import android.os.Bundle
 
+import androidx.appcompat.app.AppCompatActivity
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.widget.R
+import org.lineageos.settings.device.R
 
 class ChargerSettingsActivity : CollapsingToolbarBaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fragmentManager
-                .beginTransaction()
-                .replace(R.id.content_frame, ChargerSettingsFragment(), TAG)
+        setContentView(R.layout.battery_care_preview)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.content_frame, ChargerSettingsFragment())
                 .commit()
+        }
     }
 
     companion object {
-        private const val TAG = "AdvancedChargerSettingsActivity"
+        private const val TAG = "ChargerSettingsActivity"
     }
 }
