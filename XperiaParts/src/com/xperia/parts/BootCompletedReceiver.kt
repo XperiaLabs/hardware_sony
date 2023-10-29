7a009427b8bd35377b6b80d741a65d6b3274ae0a
saku-bruh <notwolfyy@gmail.com>
2023-10-30 01:42:05 +0200

    XperiaParts: Introduce DIRAC Audio Engine
    * Imported from:
    https://github.com/amanrajOO7/android_device_xiaomi_sdm845-common/tree/54289be8794992e03c6f883974ebeb64ea1ea42e/parts
    
    * Fix for DIRAC Service not initializing correctly:
    https://github.com/Maxx12211/device2_xiaomi_rova/commit/37fe7b65ef066a991338550486277dfae34fa7a0
    
    * DIRAC Icon converted from a SVG > Vector Drawable (by @saku-bruh)
    
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- XperiaParts/src/com/xperia/parts/BootCompletedReceiver.kt
+++ XperiaParts/src/com/xperia/parts/BootCompletedReceiver.kt
@@ -11,15 +11,28 @@ import android.content.Intent
 import android.util.Log
 import com.xperia.parts.display.CreatorModeUtils
 import com.xperia.parts.charger.ChargerUtils
+import com.xperia.parts.dirac.DiracUtils
 
 class BootCompletedReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(TAG, "Starting")
         CreatorModeUtils(context).initialize()
         ChargerUtils(context).applyOnBoot()
+
+        if (DEBUG) Log.d(TAG, "Received boot completed intent")
+
+        val millis = 1 * 60 * 1000  // 1min
+        try {
+            Thread.sleep(millis.toLong())
+            DiracUtils(context).onBootCompleted()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     companion object {
         private const val TAG = "XperiaParts"
+        private const val DEBUG = false
     }
 }
